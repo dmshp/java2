76df346511bf74e8ef4a32c228929d1cc7f3ff60
dmshp <1dimashariprasetyo@gmail.com>
2023-03-03 09:01:21 +0700

    a

--- perulangan/praktik/ww.java
+++ perulangan/praktik/ww.java
@@ -2,8 +2,8 @@ package perulangan.praktik;
 
 public class ww {
     public static void main(String[] args) {
-        for(int i=1; i<=4; i++) {
-            for(int j=4-i; j>0; j--) {
+        for(int i=1; i<=5; i++) {
+            for(int j=5-i; j>0; j--) {
                 System.out.print("  ");
             }
             for(int k=1; k<=i; k++) {
